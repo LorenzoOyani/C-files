9e0d4d9d32ced60d3b7497afce59b2cfa1a641c3
Jerumeh Lawrence <jerumehlawrence@gmail.com>
2022-09-22 12:22:50 +0100

    C-files
    cs50 worked examples

---
+++ copy.c
@@ -0,0 +1,13 @@
+// #include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+int numbers [] = {4,5,6};
+printf("%p\n",s);
+printf("%c\n",s[0]);
+printf("%i\n",*numbers);
+printf("%i\n",*(numbers + 1));
+printf("%i\n",*(numbers + 2));
+
+}
