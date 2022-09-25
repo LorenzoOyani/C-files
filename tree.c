477923b130d0ba43468615e347ef35ad6e5b6ad0
Jerumeh Lawrence <jerumehlawrence@gmail.com>
2022-09-26 00:29:12 +0100

    new changes

---
+++ tree.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool search(node *tree, int number)
+{
+    if(tree ==NULL)
+    {
+        return false;
+    }
+    else if(number < tree->number)
+    {
+        return search(tree->left, number);
+    }
+    else if ( number > tree->number)
+    {
+        return search(tree->right, number);
+    }
+    else
+    {
+        return true;
+    }
+}
